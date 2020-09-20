7a76a3b62a129d5a5dc97667ab064ff15dead91b
daniel cohen <daniel7777cohen@gmail.com>
2020-09-20 03:03:45 +0300

    removed localhost

--- api.ts
+++ api.ts
@@ -3,12 +3,12 @@ import { Todo, Note, TodoForm } from "./interfaces";
 
 export const fetchNotes = async () => {
   try {
-    const res = await axios.get("http://localhost:3000/api/notes");
+    const res = await axios.get("/api/notes");
     let processedNotes = [] as any;
     if (res.data.success === true) {
       for (const note of res.data.notes) {
         const todoReponse = await axios.get(
-          `http://localhost:3000/api/items/get-item-by-note-id/${note._id}`
+          `/api/items/get-item-by-note-id/${note._id}`
         );
         const { title, createdAt, updatedAt, _id } = note;
         const todos = todoReponse.data.todos;
@@ -24,7 +24,7 @@ export const fetchNotes = async () => {
 };
 
 export const createNewNote = async (title: string, todos: TodoForm[]) => {
-  const noteResponse = await axios.post("http://localhost:3000/api/notes", {
+  const noteResponse = await axios.post("/api/notes", {
     title,
   });
   debugger;
@@ -46,7 +46,7 @@ const getProcessedNewTodos = async (todos: TodoForm[], noteId: string) => {
   for (const todo of todos) {
     const { description, checked } = todo;
     const todoResponse = await axios.post(
-      `http://localhost:3000/api/items/add-item-to-note/${noteId}`,
+      `/api/items/add-item-to-note/${noteId}`,
       {
         description,
         checked,
@@ -61,44 +61,24 @@ const getProcessedNewTodos = async (todos: TodoForm[], noteId: string) => {
   return processedNewTodos;
 };
 export const getNoteById = async (note_id: string) => {
-  const res = await axios.get(`http://localhost:3000/api/notes/${note_id}`);
+  const res = await axios.get(`/api/notes/${note_id}`);
   return res.data;
 };
 
 export const getTodosByNoteId = async (note_id: string) => {
-  const res = await axios.get(
-    `http://localhost:3000/api/items/get-item-by-note-id/${note_id}`
-  );
+  const res = await axios.get(`/api/items/get-item-by-note-id/${note_id}`);
   return res.data;
 };
 
 export const deleteNote = async (note_id: string) => {
-  const res = await axios.delete(`http://localhost:3000/api/notes/${note_id}`);
+  const res = await axios.delete(`/api/notes/${note_id}`);
   return res.data;
-};
-
-export const getTodos = async (): Promise<string[]> => {
-  return fetch("https://localhost:4000/todos").then((res) => res.json());
-};
-
-export const postTodos = (todos: string[]) => {
-  fetch("http://localhost:4000/notes", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(todos),
-  });
 };
 
 export const saveTodosStatuses = async (todos: Todo[]) => {
   for (const todo of todos) {
-    const todoResponse = await axios.put(
-      `http://localhost:3000/api/items/edit-item/${todo._id}`,
-      {
-        checked: todo.checked,
-      }
-    );
+    const todoResponse = await axios.put(`/api/items/edit-item/${todo._id}`, {
+      checked: todo.checked,
+    });
   }
 };
